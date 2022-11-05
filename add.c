ecc41dad2bb047da5b8638bbb2a196e10a43ef5d
gaikwadrutu <gaikwadrutu36@gmail.com>
2022-11-05 20:02:25 +0530

    changes committed

--- add.c
+++ add.c
@@ -3,16 +3,11 @@
 int add(int n1, int n2){    
     int sum;
     sum = n1 + n2;
-    printf("Addition = %d\n", sum);    
+    printf("First value: 64 & Second value: 89");
+    printf("\nAddition = %d\n", sum);    
 }
 
 void main(){  
-  int n1, n2;
-  printf("Enter first value: ");
-  scanf("%d", &n1);
-  
-  printf("Enter second value: ");
-  scanf("%d", &n2);
-  
+  int n1 = 64, n2 = 89;
   add(n1, n2);
 }

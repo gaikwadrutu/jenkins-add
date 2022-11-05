0539e2110d4fa47a614f3da3390d2ad0c12bcc3a
gaikwadrutu <117518995+gaikwadrutu@users.noreply.github.com>
2022-11-05 22:29:35 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,7 @@
 #include<stdio.h>
 #include"header.h"
 
-int add(int n1, int n2){
-    
-    int sum;
-    sum = n1 + n2;
-    printf("\nAddition = %d\n", sum);
-    
+void main(){  
+  int n1 = 64, n2 = 89;
+  add(n1, n2);
 }
-
